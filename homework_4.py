85b97a3bd4309ddbee3304a798f7f613f2ad70b6
AlexanderV823 <volodin_aa@hotmail.com>
2025-10-15 21:56:41 +0300

    Добавлено решение Задачи 2 ДЗ 4

--- homework_4.py
+++ homework_4.py
@@ -20,3 +20,15 @@ for visit_dict in geo_logs:
             if location == 'Россия':
                 only_russia_geo_logs.append({visit_id: locations})
 print(only_russia_geo_logs)
+
+# Задача 2
+
+ids = {'user1': [213, 213, 213, 15, 213],
+       'user2': [54, 54, 119, 119, 119],
+       'user3': [213, 98, 98, 35]}
+
+unic_ids = set()
+
+for user_id, list  in  ids.items():
+    unic_ids.update(list)
+print(unic_ids)
