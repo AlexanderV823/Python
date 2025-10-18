ea1ab3cfb2eeac5c16f490dbc3e119e53b16974f
AlexanderV823 <volodin_aa@hotmail.com>
2025-10-19 00:14:38 +0300

    Добавлено решение Задачи 2 к Уроку 5 (Функции)

--- homework_5.py
+++ homework_5.py
@@ -31,6 +31,7 @@ def print_shelf(doc_number: str):
                     print(f'Документ находится на полке: {shelf_id}')
             if not shelf_found:
                 print(f'Место хранения документа отсутствует.')
+                break
     if not doc_found:
         print(f'Документ с указанным номером не найден.')
     
@@ -41,9 +42,55 @@ def print_list():
 def put_document(dict_doc: dict, shelf_id: str):
     if shelf_id in directories:
         documents.append(dict_doc)
-        print(f'Документ не добавлен на полку: {shelf_id}')
+        shelf_list = directories.get(shelf_id)
+        shelf_list.append(dict_doc.get("number"))
+        print(f'Документ добавлен на полку: {shelf_id}')
     else:
         print(f'Документ не добавлен в каталог. Указанной полки не существует.')
+
+def del_document(doc_number: str):   
+    doc_found = False
+    shelf_found = False
+    for document in documents:
+        if document.get("number") == doc_number:
+            doc_found = True            
+            for shelf_id, shelf_list in directories.items():
+                if doc_number in shelf_list:
+                    shelf_found = True
+                    shelf_list.remove(doc_number)
+                    del(documents[documents.index(document)])
+                    print(f'Документ с номером "{document.get("number")}" удален из каталога и с полки: "{shelf_id}"')
+                    continue   
+            if not shelf_found:
+                print(f'Место хранения документа отсутствует.')
+                break            
+    if not doc_found:
+        print(f'Документ с указанным номером не найден.') 
+
+def move_document(doc_number: str, shelf_id: str):
+    doc_found = False
+    for document in documents:
+        if document.get("number") == doc_number:
+            doc_found = True
+            for old_shelf_id, shelf_list in directories.items():
+                if doc_number in shelf_list:
+                    shelf_found = True
+                    shelf_list.remove(doc_number)
+                    new_shelf_list = directories.get(shelf_id)
+                    new_shelf_list.append(doc_number)
+                    print(f'Документ с номером {doc_number} перемещен с полки: {old_shelf_id} на полку: {shelf_id}')
+                    break
+            if not shelf_found:
+                print(f'Место хранения документа отсутствует.')
+                break
+    if not doc_found:
+        print(f'Документ с указанным номером не найден.')
+
+def add_shelf(shelf_id: str):
+    if shelf_id not in directories:
+        directories.setdefault(shelf_id, [])
+    else:
+        print(f'Указанная полка уже существует.')
 
 def print_help():
     print(f'p – people – команда, которая спросит номер документа и выведет имя человека, которому он принадлежит.')
@@ -60,23 +107,25 @@ def main():
     while True:
         user_input = input('Введите команду: ')
         if user_input == 'p':
-            print_people(input('Введите номер документа: '))
+            print_people(str(input('Введите номер документа: ')))
         elif user_input == 's':
-            print_shelf(input('Введите номер документа: '))
+            print_shelf(str(input('Введите номер документа: ')))
         elif user_input == 'l':
              print_list()
         elif user_input == 'a':          
-            new_doc = {"type": str(input('Введите номер документа: ')), "number": str(input('Введите тип документа: ')), "name": str(input('Введите имя владельца документа: '))}
+            new_doc = {"type": str(input('Введите тип документа: ')), "number": str(input('Введите номер документа: ')), "name": str(input('Введите имя владельца документа: '))}
             put_document(new_doc, str(input('Введите номер полки: ')))
-        # elif user_input == 'd':
-
-        # elif user_input == 'm':
-
-        # elif user_input == 'as':
-        
+        elif user_input == 'd':
+            del_document(str(input('Введите номер документа: ')))
+        elif user_input == 'm':
+            move_document(str(input('Введите номер документа: ')), str(input('Введите номер полки: ')))
+        elif user_input == 'as':
+            add_shelf(str(input('Введите номер новой полки: ')))
         elif user_input == 'h':
             print_help()
         elif user_input == 'q':
             break
+        else:
+            print(f'Указана не существующая команда. Для вызова справки введите "h"')
 
 main()
