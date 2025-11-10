f643d820585b1defd01d3f1f9c55bea71121c500
AlexanderV823 <volodin_aa@hotmail.com>
2025-11-10 22:55:10 +0300

    Добавлено решение Задачи 1 ДЗ 8

--- homework_8.py
+++ homework_8.py
@@ -1,27 +1,25 @@
 # Задача 1
+from pprint import pprint
 
-def read_cook_book():
-
-    file = open('recipes.txt')
-    
+def print_cook_book():
     cook_book = dict()
+    with open('recipes.txt', 'r', encoding='UTF-8') as file:      
+        line = file.readline().strip()
+        while line:
+                try:
+                    dish = line
+                    count_ingredients = int(file.readline().strip())
+                    ingredients = list()
+                    for _ in range(count_ingredients):
+                        ingredient_string = file.readline().strip()
+                        ingredient_summary = ingredient_string.split(' | ')
+                        ingredient_info = {'ingredient_name': ingredient_summary[0], 'quantity': int(ingredient_summary[1]), 'measure': ingredient_summary[2]}
+                        ingredients.append(ingredient_info)
+                    cook_book.setdefault(dish, ingredients)
+                    line = file.readline().strip()  # Здесь возникает пустая строка, если ее здесь не перескочить, то условие прервется раньше конца файла
+                    line = file.readline().strip()
+                except ValueError:
+                    line = file.readline().strip()       
+    pprint(cook_book)
 
-    lines_list = file.readlines()
-
-    for index, line in enumerate(lines_list):
-        if line:
-            if isinstance(line, str) and isinstance(lines_list[index::1], int):
-                dish = line.strip()
-                print(dish)
-                ingrdnt_list = list()
-                first_ingrdnts_index = index + 2
-                print(first_ingrdnts_index)
-                while first_ingrdnts_index <= lines_list[index + 1]:
-                    print(first_ingrdnts_index)
-                    ingredients = lines_list[first_ingrdnts_index].split(' | ') 
-                    print(ingredients)            
-                    ingrdnt_list.append({'ingredient_name': ingredients[0], 'quantity': ingredients[1], 'measure': ingredients[2]})
-                    cook_book.setdefault(dish, ingrdnt_list)
-    print(cook_book)
-
-read_cook_book()
+print_cook_book()
