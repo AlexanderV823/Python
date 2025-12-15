2a847522dbcf69e9832217548d0167b8be7e09bb
AlexanderV823 <volodin_aa@hotmail.com>
2025-12-15 23:42:14 +0300

    Добавлено эталонное решение Задачи 1 и 2 ДЗ 10

--- homework_10.py
+++ homework_10.py
@@ -23,6 +23,26 @@ max_intelligence = max(heroes_dict.values())
 
 print(f'Супергерой с максимальным интеллектом: {max_intelligence_hero} {max_intelligence}')
 
+# Эталон
+
+import requests
+def get_smart_superhero(names):
+    top_intelligence = -1
+    top_name = ''
+    for name in names:
+        r = requests.get(f'https://www.superheroapi.com/api.php/2619421814940190/search/{name}')
+        data = r.json()
+        intelligence = int(data['results'][0]['powerstats']['intelligence'])
+        print(name, intelligence)
+        if intelligence > top_intelligence:
+            top_intelligence = intelligence
+            top_name = name
+
+    print(f'{top_name} - самый умный, его интеллект равен {top_intelligence}.')
+
+
+get_smart_superhero(['Hulk', 'Captain America', 'Thanos'])
+
 # Задача 2
 
 import requests
@@ -54,3 +74,39 @@ with open(full_path, 'rb') as f:
         raise Exception("Ответ не 201!")
             
     print('Файл загружен!')
+
+    # Эталон
+
+    import requests
+import os
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        my_headers = {'Authorization': self.token}
+        fname = os.path.split(file_path)[1]
+        my_params = {'path': '/' + fname, 'overwrite': 'true'}
+        url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+        r = requests.get(url, headers=my_headers, params=my_params)
+
+        if r.status_code != requests.codes.ok:
+            return f'При получении ссылки для загрузки произошла ошибка (код: {r.status_code})'
+
+        href = r.json()['href']
+        with open(file_path, 'rb') as fh:
+            r = requests.put(href, data=fh.read())
+
+        if r.status_code not in (requests.codes.created, requests.codes.accepted):
+            return f'При загрузке файла произошла ошибка (код: {r.status_code})'
+        return f'Файл {fname} успешно загружен на Яндекс.Диск'
+
+
+if __name__ == '__main__':
+    my_file_path = 'result.txt'
+    auth_token = ''
+    uploader = YaUploader(auth_token)
+    result = uploader.upload(my_file_path)
+    print(result)
